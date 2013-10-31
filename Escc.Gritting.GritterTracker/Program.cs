a6b8db922a094181ac3d7946c8538d4fe65cb3b4
Rick Mason <richard.mason@eastsussex.gov.uk>
2013-10-31 15:34:27 +0000

    Add empty gritter tracker project

---
+++ Escc.Gritting.GritterTracker/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Escc.Gritting.GritterTracker
+{
+    /// <summary>
+    /// Read current data on the positions of gritters
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
