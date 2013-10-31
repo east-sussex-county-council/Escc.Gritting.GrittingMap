d485732eddb0d24643578c2864b204b9a51dbf09
Rick Mason <richard.mason@eastsussex.gov.uk>
2013-10-31 14:47:18 +0000

    Add AJAX data source

---
+++ GritterData.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="GritterData.ashx.cs" Class="Escc.Gritting.GrittingMap.GritterData" %>
---
+++ GritterData.ashx.cs
@@ -0,0 +1,58 @@
+﻿using System.Web;
+using Escc.Gritting.Fakes;
+
+namespace Escc.Gritting.GrittingMap
+{
+    /// <summary>
+    /// Provide data on gritters data as JSON
+    /// </summary>
+    public class GritterData : IHttpHandler
+    {
+        /// <summary>
+        /// Enables processing of HTTP Web requests by a custom HttpHandler that implements the <see cref="T:System.Web.IHttpHandler"/> interface.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.Web.HttpContext"/> object that provides references to the intrinsic server objects (for example, Request, Response, Session, and Server) used to service HTTP requests.</param>
+        public void ProcessRequest(HttpContext context)
+        {
+            var repo = new FakeGritterRepository();
+
+            WriteResponseAsJson(context, repo);
+        }
+
+        /// <summary>
+        /// Writes the response as JSON.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="repo">The repo.</param>
+        private static void WriteResponseAsJson(HttpContext context, IGritterDataRepository repo)
+        {
+            context.Response.ContentType = "text/javascript";
+            context.Response.Write("[");
+            var first = true;
+            foreach (var gritter in repo.ReadAllGritters())
+            {
+                if (!first)
+                {
+                    context.Response.Write(",");
+                }
+                first = false;
+
+                context.Response.Write("{\"id\":\"" + gritter.GritterId + "\",\"name\":\"" + gritter.GritterName + "\",\"lat\":" + gritter.Latitude + ",\"lng\":" + gritter.Longitude + "}");
+            }
+            context.Response.Write("]");
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether another request can use the <see cref="T:System.Web.IHttpHandler"/> instance.
+        /// </summary>
+        /// <value></value>
+        /// <returns>true if the <see cref="T:System.Web.IHttpHandler"/> instance is reusable; otherwise, false.</returns>
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
