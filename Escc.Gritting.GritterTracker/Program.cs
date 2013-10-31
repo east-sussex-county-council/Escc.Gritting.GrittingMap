f8e4cc99577c73f11a791829fa36ffd1fc640b24
Rick Mason <richard.mason@eastsussex.gov.uk>
2013-10-31 16:19:37 +0000

    Update SQL Server with new gritter data

--- Escc.Gritting.GritterTracker/Program.cs
+++ Escc.Gritting.GritterTracker/Program.cs
@@ -1,13 +1,29 @@
 ﻿
+using System;
+using Escc.Gritting.Fakes;
+using Escc.Gritting.SqlServer;
+
 namespace Escc.Gritting.GritterTracker
 {
     /// <summary>
-    /// Read current data on the positions of gritters
+    /// Update data on the positions of gritters from the data source to the repository used by the UI
     /// </summary>
     class Program
     {
         static void Main()
         {
+            UpdateGritterData(new FakeGritterRepository(), new SqlServerGritterRepository());
+        }
+
+        private static void UpdateGritterData(IGritterDataRepository fromRepository, IGritterDataRepository toRepository)
+        {
+            if (fromRepository == null) throw new ArgumentNullException("fromRepository");
+            if (toRepository == null) throw new ArgumentNullException("toRepository");
+
+            foreach (var gritter in fromRepository.ReadAllGritters())
+            {
+                toRepository.SaveGritter(gritter);
+            }
         }
     }
 }
