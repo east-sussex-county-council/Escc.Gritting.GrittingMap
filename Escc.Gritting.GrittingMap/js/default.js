4f4337e2dcd2b7d43cc32babdc0d383f520891ca
Rick Mason <richard.mason@eastsussex.gov.uk>
2013-11-21 14:24:12 +0000

    Add marker clustering and move libraries to eastsussex.gov.uk project

--- Escc.Gritting.GrittingMap/js/default.js
+++ Escc.Gritting.GrittingMap/js/default.js
@@ -2,16 +2,17 @@
 
     var esccGrittingMap = function () {
         /// <summary>Application to display the location of gritters on a map. This function forms the namespace.</summary>
-        var map, markers = {};
+        var map, markers = {}, clusterer;
 
         function setupApplication() {
             /// <summary>Set up the application and load the initial data</summary>
             $.ajax({
-                url: "js/libs/markerwithlabel_packed.js",
+                url: "/js/googlemapsmarkerwithlabel-googlemapsmarkerclusterer.jsx",
                 dataType: "script",
                 cache: true,
                 success: function () {
                     map = esccGoogleMaps.createMap();
+                    clusterer = new MarkerClusterer(map);
                     esccGoogleMaps.addLocationSearch('google-location', 'google-search', map);
                     loadGritters();
                 }
@@ -21,15 +22,15 @@
         function loadGritters() {
             /// <summary>Loads gritter data and creates or updates markers</summary>
             $.getJSON("gritterdata.ashx", function (data, status) {
-               var gritters = data.length;
+                var gritters = data.length;
                 for (var i = 0; i < gritters; i++) {
-                     if (markers[data[i].id] === undefined) {
+                    if (markers[data[i].id] === undefined) {
                         markers[data[i].id] = new MarkerWithLabel({
-                            map: map,
                             icon: "img/gritter.png",
                             labelClass: "marker-label",
                             labelContent: data[i].name
                         });
+                        clusterer.addMarker(markers[data[i].id]);
                     }
                     markers[data[i].id].setPosition(new google.maps.LatLng(data[i].lat, data[i].lng));
                 }
